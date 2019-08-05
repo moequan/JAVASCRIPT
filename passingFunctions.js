5ac398de0f0b1288d43023083beb1bac93039ab0
moequan <maurice.s93@web.de>
2019-08-05 14:03:48 +0200

    Adding other posibillity for passing function.

--- passingFunctions.js
+++ passingFunctions.js
@@ -1,9 +1,29 @@
 //PASSING FUNCTIONS AS ARGUMENTS
 
+
+//FIRST SOLUTION
+
+// let name = "Maurice";
+// function detailsCalculate(name,birthYear, calculateAge)
+// {
+//     console.log(`Hi, ${name} your age is ${calculateAge(birthYear)}`);
+// }
+
+// function calculateAge(ageParam)
+// {
+//     return 2019-ageParam;
+// }
+// console.log("Age...");
+// detailsCalculate(name,1993,calculateAge)
+
+
+
+//OTHER SOLUTION TO PASS FUNCTIONS
+
 let name = "Maurice";
-function detailsCalculate(name,birthYear, calculateAge)
+function detailsCalculate(name,birthYear, calcAge)
 {
-    console.log(`Hi, ${name} your age is ${calculateAge(birthYear)}`);
+    console.log(`Hi, ${name} your age is ${calcAge(birthYear)}`);
 }
 
 function calculateAge(ageParam)
