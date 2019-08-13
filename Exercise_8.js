3f2a964e8c859e64ca2731d3b3376f90f356692d
moequan <maurice.s93@web.de>
2019-08-13 13:19:19 +0200

    Solving Exercise ( Return snake case to camel case).

--- Exercise_8.js
+++ Exercise_8.js
@@ -5,13 +5,59 @@
 //unique([0, 1, 1, 1, 1, 1, 1, 1]) ➞ 0
 
 
-function uniqueNum (nums){
-    let uniqueArr = [];
-   for (i = 0; i < nums.length; i++){
-       if(nums.indexOf(nums[i]) === nums.lastIndexOf(nums[i]))
-       uniqueArr.push(nums[i])     
-   }  
-   console.log(uniqueArr)
-}
-uniqueNum([0, 0, 0.77, 0, 0,5,4,3,10])
-uniqueNum(['pigs','pigs','pigs',`goats`,`tiger`,`lion`])
+// function uniqueNum (nums){
+//     let uniqueArr = [];
+//    for (i = 0; i < nums.length; i++){
+//        if(nums.indexOf(nums[i]) === nums.lastIndexOf(nums[i]))
+//        uniqueArr.push(nums[i])     
+//    }  
+//    console.log(uniqueArr)
+// }
+// uniqueNum([0, 0, 0.77, 0, 0,5,4,3,10])
+// uniqueNum(['pigs','pigs','pigs',`goats`,`tiger`,`lion`])
+
+//Create a function toCamelCase() that takes a single string and convert it into camelCase from snake_case.
+//Examples:
+//toCamelCase("hello_world") ➞ "helloWorld"
+//toCamelCase("javascript_is_fun") ➞ "javaScriptIsFun"
+
+
+// function toCamelCase (str){
+
+
+//    var str = "hello_world";
+//    str.split(``)
+
+   
+// var camelStr = str.replace(/_/i, "W");
+// camelStr = camelStr.split(`w`)
+// camelStr = camelStr.join(``)
+   
+//    console.log(camelStr)
+// }
+
+// toCamelCase("hello_world")
+
+
+// let strFirst = str.split(` `)
+
+//     for (var i = 0; i < strFirst.length; i++) {
+//         strFirst[i] = strFirst[i].charAt(0).toUpperCase() + strFirst[i].substring(1)
+//     }
+//     strFirst = strFirst.join(' ');
+//     console.log(strFirst)
+// }
+
+
+function toCamelCase (str){
+let strSecond = str.split(`_`)
+for (var i = 1; i < strSecond.length; i++) {
+      
+strSecond[i] = strSecond[i].charAt(0).toUpperCase() + strSecond[i].substring(1)
+ }
+ 
+    strSecond = strSecond.join(``)
+    console.log(strSecond)
+ }
+ toCamelCase("hello_world")
+ toCamelCase("javascript_is_fun")
